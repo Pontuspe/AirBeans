2deccd80567a382e06661e88a06675ccd1421620
Pontuspe <pontus-pettersson@hotmail.com>
2021-02-01 14:35:21 +0100

    wip

---
+++ airbeans/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*, *:after, *:before{
+  margin:0;
+  padding:0;
+}
+html, body{
+  height:100%;
+  overflow-x:hidden;
+}
+:root{
+  --AirBeanGreen: #0e927d;
+  --AirBeanBrown: #2f2926;
+  --AirBeanPink: #f3e4e1;
+  --AirBeanOrange: #e5674e;
+}
+</style>
---
+++ airbeans/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="container">
+    <img src="@/assets/intro-graphic-left.svg" alt="Beautiful leafs on the left">
+    <img src="@/assets/airbean-landing.svg" alt="AirBean logo and text">
+    <img src="@/assets/intro-graphic-right.svg" alt="Beautiful leafs on the right">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    img:nth-of-type(){
+        height:100%;
+    }
+}
+
+</style>
+
+<style lang="scss">
+body{
+    background-color: var(--AirBeanGreen);
+}
+</style>
